6b1614fc4213fcb28cecba9fee6804c040a1b50f
Hung <hungamtp@gmail.com>
2022-04-23 08:24:12 +0700

    hung

--- src/main/java/com/example/demodocker/HomeController.java
+++ src/main/java/com/example/demodocker/HomeController.java
@@ -11,4 +11,9 @@ public class HomeController {
     public String hello(){
         return "<h1 style=\"color:blue;\">hung<h1>";
     }
+    @GetMapping("/fr")
+    public String fr(){
+        return
+            "<img src=\"https://firebasestorage.googleapis.com/v0/b/mroki-a39d3.appspot.com/o/D8956427-11EE-4EE2-A229-54E92360D6A8.jpeg?alt=media&token=4bf845cb-d1b3-4f19-83ee-e32849343e2c alt=\"hung\"/>";
+    }
 }
